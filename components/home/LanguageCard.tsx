a38b0ce5310a70df5ca63ff1b5fc708f64b8577e
Angelillo15 <contact@angelillo15.es>
2024-01-20 12:56:52 +0100

    Change background color

--- components/home/LanguageCard.tsx
+++ components/home/LanguageCard.tsx
@@ -1,13 +1,13 @@
 import { Tooltip } from '@nextui-org/react';
-import { LanguageCard } from './Languages';
+import { LanguageCardProps } from './Languages';
 import { Icon } from '@iconify/react';
 import { motion } from 'framer-motion';
 import { item } from './Header';
 
-const LanguageCard = ({ icon, tooltip }: LanguageCard) => {
+const LanguageCard = ({ icon, tooltip }: LanguageCardProps) => {
   return (
-    <motion.div variants={item} className='flex items-center justify-center w-20 h-20 rounded-full text-3xl bg-gray-200 dark:bg-gray-700 hover:text-purple-400 transition-colors'>
-      <Tooltip content={tooltip} placement='bottom'>
+    <motion.div variants={item} className='flex items-center justify-center w-20 h-20 rounded-full text-3xl bg-gray-200 dark:bg-[#171520] hover:text-purple-400 transition-colors'>
+      <Tooltip offset={40} content={tooltip} placement='bottom'>
         <Icon icon={icon} />
       </Tooltip>
     </motion.div>
