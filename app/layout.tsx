9c9692840270bd38c2a94fc4fc5a2085703eb9e0
Angelillo15 <contact@angelillo15.es>
2024-01-20 12:55:31 +0100

    Add Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import '@/app/globals.css';
 import { Providers } from './providers';
 import NavBar from '@/components/NavBar';
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Angelillo15 - 😈',
+  description: 'An spanish developer who loves to code and learn new things.',
+  keywords: 'Angelillo15, Angelillo, Angel Martin Gonzalez, Nookure, Java Developer, MAStaff, Pterodactyl themes',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
