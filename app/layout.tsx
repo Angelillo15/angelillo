62f15e8918b9d3d3eafccad1bd0fed7428489fc4
Angelillo15 <contact@angelillo15.es>
2024-02-05 21:31:04 +0100

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta property='og:image' content='/preview.png' />
         <meta property='og:image:width' content='843' />
         <meta property='og:image:height' content='480' />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
         <Providers>
