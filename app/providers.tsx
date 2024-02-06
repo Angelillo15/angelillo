94f2b15bd3d3d83df7200af94627d47be720a7c1
Angelillo15 <contact@angelillo15.es>
2024-02-06 16:51:57 +0100

    Add links

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export function Providers({children}: { children: React.ReactNode }) {
+  const router = useRouter()
+
   return (
-    <NextUIProvider className='m-2'>
+    <NextUIProvider className='m-2' navigate={router.push}>
       {children}
     </NextUIProvider>
   )
--- components/home/HomeProjects.tsx
+++ components/home/HomeProjects.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@nextui-org/react';
 import Image from 'next/image';
 import MAStaffImage from '@/assets/plugins/mastaff.png';
-import NookTheme from '@/assets/themes/nooktheme.png'
+import NookTheme from '@/assets/themes/nooktheme.png';
 import { Variants, motion } from 'framer-motion';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export const container: Variants = {
   hidden: { opacity: 1, scale: 0 },
@@ -11,21 +13,29 @@ export const container: Variants = {
     scale: 1,
     transition: {
       delayChildren: 0.5,
-      staggerChildren: 0.5
-    }
-  }
-}
-  
+      staggerChildren: 0.5,
+    },
+  },
+};
+
 export const item: Variants = {
   hidden: { y: 20, opacity: 0 },
   visible: {
     y: 0,
-    opacity: 1
-  }
-}
+    opacity: 1,
+  },
+};
+
 const HomeProjects = () => {
+  const router = useRouter();
   return (
-    <motion.section variants={container} initial='hidden' animate='visible' id='facts' className='flex flex-col max-w-7xl mx-auto px-4 py-8 gap-8 md:gap-12 md:py-16'>
+    <motion.section
+      variants={container}
+      initial='hidden'
+      animate='visible'
+      id='facts'
+      className='flex flex-col max-w-7xl mx-auto px-4 py-8 gap-8 md:gap-12 md:py-16'
+    >
       <motion.div variants={item} className='flex flex-col gap-6 md:flex-row-reverse md:gap-8 xl:gap-24 items-center'>
         <div className='w-full flex-1 rounded-xl bg-gray-900 aspect-video relative overflow-clip'>
           <Image
@@ -40,12 +50,29 @@ const HomeProjects = () => {
         <div className='flex-1'>
           <h2 className='font-semibold text-2xl md:text-4xl'>Creating Minecraft plugins</h2>
           <p className='md:mt-6 md:text-xl text-gray-900 dark:text-gray-100 mt-3'>
-            I&apos;m a Minecraft plugin developer, I have been developing plugins for 3 years, I have a lot of experience in
-            this sector. I&apos;m currently working on <span className='purple-glow-gradient'> MAStaff </span> and my brand new
-            <span className='red-glow-gradient'> Nookure Chat</span>.
+            I&apos;m a Minecraft plugin developer, I have been developing plugins for 3 years, I have a lot of
+            experience in this sector. I&apos;m currently working on{' '}
+            <Link
+              href={'https://builtbybit.com/resources/mastaff-staffmode-utils.25460/'}
+              className='purple-glow-gradient'
+            >
+              {' '}
+              MAStaff{' '}
+            </Link>{' '}
+            and my brand new
+            <Link href={'https://github.com/Nookure/NookureChat'} className='red-glow-gradient'>
+              {' '}
+              Nookure Chat
+            </Link>
+            .
           </p>
           <div className='flex flex-row gap-4 mt-8'>
-            <Button color='secondary' href='/resoruces'>
+            <Button
+              color='secondary'
+              onClick={() => {
+                router.push('resources');
+              }}
+            >
               Check out Resources
             </Button>
           </div>
@@ -67,11 +94,24 @@ const HomeProjects = () => {
           <h2 className='font-semibold text-2xl md:text-4xl'>Developing for Pterodactyl</h2>
           <p className='md:mt-6 md:text-xl text-gray-900 dark:text-gray-100 mt-3'>
             I&apos;m glad to say that I lead the Open Source development of Pterodactyl Themes, I have projects like
-            <span className='red-glow-gradient'> NookTheme</span> or mor simple themes that works on every version of
-            pterodactyl like <span className='purple-glow-gradient'>Ice Minecraft theme</span>.
+            <Link
+              href={'https://builtbybit.com/resources/nooktheme-pterodactyl-theme.30893/'}
+              className='red-glow-gradient'
+            >
+              {' '}
+              NookTheme
+            </Link>{' '}
+            or mor simple themes that works on every version of pterodactyl like{' '}
+            <Link
+              href={'https://builtbybit.com/resources/iceminecrafttheme-pterodactyl-theme.26274/'}
+              className='purple-glow-gradient'
+            >
+              Ice Minecraft theme
+            </Link>
+            .
           </p>
           <div className='flex flex-row gap-4 mt-8'>
-            <Button color='secondary' href='/resoruces'>
+            <Button color='secondary' href='/resoruces' onClick={() => router.push('/resources')}>
               Check out Themes
             </Button>
           </div>
